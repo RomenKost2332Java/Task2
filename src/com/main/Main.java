14cc586fc73beac5360411ec696e7172428bec18
RomenKost2332Java <romenl@gmail.com>
2021-07-06 06:20:00 +0300

    Updated method "main". Realized bubble sort algorithm.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,6 +3,27 @@ package com.main;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final int size = 100;
+
+        double[] array = new double[size];
+
+        for(int i = 0; i<array.length; i++)
+            array[i] = Math.random()/Math.random();
+
+        double[] bubbleSortedArray = array;
+        double buffer;
+        for (int i = 0; i<bubbleSortedArray.length-1; i++){
+            for (int j = 0; j<bubbleSortedArray.length-1-i; j++){
+                if (bubbleSortedArray[j] > bubbleSortedArray[j+1]){
+                    buffer = bubbleSortedArray[j];
+                    bubbleSortedArray[j] = bubbleSortedArray[j+1];
+                    bubbleSortedArray[j+1] = buffer;
+                }
+            }
+        }
+
+        for (double e : bubbleSortedArray){
+            System.out.println(e);
+        }
     }
 }
